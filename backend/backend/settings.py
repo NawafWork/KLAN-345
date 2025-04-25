a3db9109fefac70e1b5b0a63947176149516fa41
NawafWork <nawafbs268@gmail.com>
2025-04-25 17:39:50 -0400

    Fix templates context_processors for Django and React

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,6 +93,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",   # 👈 Add this line
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -185,7 +186,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files
 STATICFILES_DIRS = [
